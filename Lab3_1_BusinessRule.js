676705c2458cf96149881912b3128f393c4af21b
greygo <grey@novoscale.com>
2017-04-17 14:52:21 -0500

    Create Lab3_1_BusinessRule.js

---
+++ Lab3_1_BusinessRule.js
@@ -0,0 +1,17 @@
+(function executeRule(current, previous /*null when async*/) {
+	try {
+		var r = new sn_ws.RESTMessageV2('Lab 3.0', 'post');
+		r.setStringParameter('username', 'Lab3.0');
+		r.setStringParameter('number', current.number);
+		r.setStringParameter('title', 'New Priority ' + current.priority + ' Incident has been created');
+		r.setStringParameter('short_description', current.short_description);
+		r.setStringParameter('requested_by', current.caller_id);
+		r.setStringParameter('url', 'https://' + gs.getProperty('instance_name') + '.service-now.com/nav_to.do?uri=incident.do?sysparm_query=number=' + current.number);
+		var response = r.execute();
+		var responseBody = response.getBody();
+		var httpStatus = response.getStatusCode();
+	}
+	catch(ex) {
+		var message = ex.getMessage();
+	}	
+})(current, previous);
