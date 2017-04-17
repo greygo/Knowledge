5f3f1f07bac326b96111720e30cfa97fc938ba89
greygo <grey@novoscale.com>
2017-04-17 14:47:56 -0500

    Create Lab3_1_Content.js

---
+++ Lab3_1_Content.js
@@ -0,0 +1,16 @@
+{
+	"username": "${username}",
+	"text": "${title}",
+	"attachments": [{
+	    "fallback": "${short_description}",
+	    "color": "#ff0000",
+	    "author_name": "${requested_by}",
+	    "title": "${number}",
+	    "text": "${short_description}"
+	},
+	{
+	    "color":"#0000ff",
+	    "title": "Click here to open ${number}",	
+	    "title_link":"${url}"
+	}]
+}
